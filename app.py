64e766a6c40efab7099dfb8f4659e1a3008cfa15
lloydoad <lloyd.lloyddapaah@gmail.com>
2019-05-09 21:28:17 -0400

    add boiler plate

--- app.py
+++ app.py
@@ -3,4 +3,12 @@ from routers.seatgeek import getSeatGeekData
 from modules.event import Event
 import routers.mongo as Database
 
-print(getSeatGeekData('miami', 30))
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/', methods=['GET'])
+def index():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run()
